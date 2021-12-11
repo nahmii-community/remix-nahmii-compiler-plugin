c1e50addd4ef478780e01bf80a523b7aede05113
Hein <heindauven@gmail.com>
2021-12-11 13:42:10 +0100

    Update interface name.

--- src/types/profile.ts
+++ src/types/profile.ts
@@ -1,7 +1,7 @@
-import { OptimismCompilerApi } from "./optimism-compiler"
+import { NahmiiCompilerApi } from "./optimism-compiler"
 import { LibraryProfile } from "@remixproject/plugin-utils"
 
-export const compilerProfile: LibraryProfile<OptimismCompilerApi> = {
+export const compilerProfile: LibraryProfile<NahmiiCompilerApi> = {
   name: "nahmii-compiler",
   methods: ["compile", "getCompilationResult"],
   events: ["compilationFinished"],
